f49037a7fde46e0f7fd4327fe2bea16a188da3c9
Ismael Castro <iscastro@es.gestamp.com>
2024-09-19 16:00:10 +0200

    jenkins

--- obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f83f3c3e5e0964791f8359f6cebd57fd331ff73d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
